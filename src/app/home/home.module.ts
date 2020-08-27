7b68ec29b57df8de5f7dcb250a2d268c2d904cfc
Manolo Perez <manolope338@gmail.com>
2020-08-27 06:00:33 -0500

    Update style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -9,10 +9,9 @@ import { SharedModule } from '../shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NebularModule } from '../nebular.module';
-import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, BrowserAnimationsModule, NebularModule, NgImageSliderModule],
+  imports: [CommonModule, SharedModule, HomeRoutingModule, BrowserAnimationsModule, NebularModule],
 })
 export class HomeModule {}
